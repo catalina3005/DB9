36ce1efea2334ee2df408585b3f643027a892586
catalina3005 <95545535+catalina3005@users.noreply.github.com>
2022-08-24 15:31:40 +0300

    Delete DB4 directory

--- DB4/src/main/java/repository/ProductsRepository.java
+++
@@ -1,9 +0,0 @@
-package repository;
-import model.Products;
-import org.springframework.data.repository.CrudRepository;
-import java.util.Optional;
-
-public interface ProductsRepository extends CrudRepository<Products, Integer> {
-    public void delete(Products product);
-    public Optional<Products> findById(Integer id);
-}
